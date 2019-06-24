a32b8441c702d133f680f29890b7d1b250dfd32b
crt <1747369444@qq.com>
2019-06-24 20:27:57 +0800

    使用本地存储存储登录信息

--- src/router/index.js
+++ src/router/index.js
@@ -3,32 +3,31 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    // {
-    //   name: 'home',
-    //   path: '/',
-    //   component: () => import('@/views/home')
-    // },
-    {
-      path: '/',
-      component: () => import('@/views/layout'),
-      children: [{
-        name: 'home',
-        path: '',
-        component: () => import('@/views/home')
-      },
-      {
-        name: 'publish',
-        path: '/publish',
-        component: () => import('@/views/publish')
-      }
-      ]
+const router = new Router({
+  routes: [{
+    // name：‘layout’由于它有默认子路由，所以它的名字没有意义，否则 Vue 会给你发黄牌警告
+    name: 'layout',
+    path: '/',
+    component: () => import('@/views/layout'),
+    // 子路由
+    children: [{
+      name: 'home',
+      path: '', // 为空为默认子路由，默认渲染到layout中
+      component: () => import('@/views/home')
     },
     {
-      name: 'login',
-      path: '/login',
-      component: () => import('@/views/login')
+      name: 'publish',
+      path: '/publish',
+      component: () => import('@/views/publish')
     }
+    ]
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/login')
+  }
   ]
 })
+
+export default router
