049392414de9c4509c6ff2904ae01657fcb8c4f3
crt <1747369444@qq.com>
2019-06-24 21:49:24 +0800

    内容列表-创建组件并配置路由

---
+++ src/views/article/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>手机阿卡京东卡登记卡记录卡就卡就卡解放路卡积分卡福建安科技发卡量精神可嘉阿里看风景奥卡福萨达撒大所大所所大撒多撒多撒多撒大多撒所大所大所大所大多所撒大付所过无过所付付群翁无群二无建安里看风景阿卡丽九分裤</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleList'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
