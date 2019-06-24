e39dd3517fa927616d16ee8bb25d448e387a453c
crt <1747369444@qq.com>
2019-06-24 21:01:46 +0800

    使用路由拦截器处理页面访问权限&nprogress处理进度条

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import './styles/index.less'
+import 'nprogress/nprogress.css'
 
 Vue.use(ElementUI)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import nprogress from 'nprogress'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [{
     // name：‘layout’由于它有默认子路由，所以它的名字没有意义，否则 Vue 会给你发黄牌警告
-    name: 'layout',
+    // name: 'layout',
     path: '/',
     component: () => import('@/views/layout'),
     // 子路由
@@ -30,4 +31,40 @@ const router = new Router({
   ]
 })
 
+/**
+ * 所有路由导航都要经过这里
+ * to 去哪儿
+ * from 来自哪里
+ * next 允许通过的方法
+ */
+
+router.beforeEach((to, from, next) => {
+  nprogress.start()
+  const userInfo = window.localStorage.getItem('user_info')
+  // 如果是非 /login 页面，判断其登录状态
+  if (to.name !== 'login') {
+    // 如果没有登录，让其跳转到登录页
+    if (!userInfo) {
+      next({
+        name: 'login'
+      })
+    } else {
+      // 如果登录了则允许通过
+      next()
+    }
+  } else {
+    // 如果登录了不允许访问登录页了
+    if (userInfo) {
+      next(false)
+    } else {
+      // 没有访问才能访问登录页
+      next()
+    }
+  }
+})
+// 路由导航完成后进入这里
+router.afterEach((to, from) => {
+  nprogress.done()
+})
+// 导出router
 export default router
